30e342d7f0f15a196f844a95af0246ced4b5d1fe
joshy24 <joshuamajebi@yahoo.com>
2021-10-27 12:31:49 +0100

    worked on admin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './NewApp';
+import App from './App';
 import Login from './components/Login'
 import Landing from './components/Landing'
 
